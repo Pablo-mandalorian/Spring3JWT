1436cdb22db869d8b1d0dac863b36b39cbd3a91f
Pablo-mandalorian <pablotelles935@gmail.com>
2023-02-14 13:59:57 -0600

    Complete create User, update user in UserService

--- src/main/java/com/pablotelles/springjwt/repositorys/UserRepository.java
+++ src/main/java/com/pablotelles/springjwt/repositorys/UserRepository.java
@@ -1,9 +1,11 @@
 package com.pablotelles.springjwt.repositorys;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pablotelles.springjwt.models.User;
 
 public interface UserRepository extends JpaRepository<User,Long>{
-    
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/com/pablotelles/springjwt/services/UserService.java
+++ src/main/java/com/pablotelles/springjwt/services/UserService.java
@@ -2,7 +2,12 @@ package com.pablotelles.springjwt.services;
 
 import com.pablotelles.springjwt.repositorys.UserRepository;
 
+import jakarta.transaction.Transactional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.pablotelles.springjwt.models.User;
 
@@ -17,15 +22,73 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    //Get Users
     public Iterable<User> getUsers(){
         return userRepository.findAll();
     }
 
-    public void getUserById(Long id){
+    //Get User by his id
+    public User getUserById(Long id){
         Optional<User> optionalUser = userRepository.findById(id);
 
         if(!optionalUser.isPresent()){
-            
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        User user = optionalUser.get();
+        return user;
+    }
+
+    //Create User
+    public User addUser(User user){
+        validateUsername(user);
+        User newUser = userRepository.save(user);
+        return newUser;
+    }
+
+    //Validate User
+    private void validateUsername(User user){
+        validateName(user.getUsername());
+        Optional<User> optionalUser = userRepository.findByUsername(user.getUsername());
+        if(optionalUser.isPresent()){
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+    }
+
+    //Validates if the displayName is empty or not
+    private void validateName(String displayName){
+        if(ObjectUtils.isEmpty(displayName)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    //Update user
+    @Transactional
+    public void updateUser(String username, String firstName, String lastName, Integer age, String email, String password){
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+
+        if(!optionalUser.isPresent()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        User userToUpdate = optionalUser.get();
+        if(username!=null){
+            userToUpdate.setUsername(username);
+        }
+        if(firstName!=null){
+            userToUpdate.setFirstName(firstName);
+        }
+        if(lastName!=null){
+            userToUpdate.setLastname(lastName);
+        }
+        if(age!=null){
+            userToUpdate.setAge(age);
+        }
+        if(email!=null){
+            userToUpdate.setEmail(email);
+        }
+        if(password!=null){
+            userToUpdate.setPassword(password);
         }
     }
 
