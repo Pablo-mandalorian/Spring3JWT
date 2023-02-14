c7aa7d3c4aa4b12f0984efd3fdfd153ed4da43df
Pablo-mandalorian <pablotelles935@gmail.com>
2023-02-14 15:49:09 -0600

    Complete User Controller CRUD

--- src/main/java/com/pablotelles/springjwt/controllers/UserController.java
+++ src/main/java/com/pablotelles/springjwt/controllers/UserController.java
@@ -3,74 +3,53 @@ package com.pablotelles.springjwt.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pablotelles.springjwt.models.User;
+import com.pablotelles.springjwt.services.UserService;
 
-import java.util.List;
-import java.util.ArrayList;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    /*
-     * $(document).ready(function(){
-     *  $('dataTable').DataTable();
-     * });
-     */
+    private final UserService userService;
+
+    UserController(final UserService userService){
+        this.userService = userService;
+    }
     
     @GetMapping
-    public List<User> getUser(@PathVariable Long id) {
-        List<User> usuarios = new ArrayList<>();
-        User newUser = new User();
-        newUser.setId(1L);
-        newUser.setUsername("Revan");
-        newUser.setFirstName("Kevin");
-        newUser.setLastname("Telles");
-        newUser.setAge(23);
-        newUser.setEmail("PabloTelles@gmail.com");
-        newUser.setPassword("spartan");
-
-        User usuario2 = new User();
-        newUser.setId(1L);
-        newUser.setUsername("Revan");
-        newUser.setFirstName("Kevin");
-        newUser.setLastname("Telles");
-        newUser.setAge(23);
-        newUser.setEmail("PabloTelles@gmail.com");
-        newUser.setPassword("spartan");
-
-        User usuario3 = new User();
-        newUser.setId(1L);
-        newUser.setUsername("Revan");
-        newUser.setFirstName("Kevin");
-        newUser.setLastname("Telles");
-        newUser.setAge(23);
-        newUser.setEmail("PabloTelles@gmail.com");
-
-        usuarios.add(newUser);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-        return usuarios;
+    public Iterable<User> getUsersIterable() {
+        return userService.getUsers();
     }
 
-    @DeleteMapping("/{id}")
-    public User deleteUser(){
-        User newUser = new User();
-        newUser.setId(1L);
-        newUser.setUsername("Revan");
-        newUser.setFirstName("Pablo");
-        newUser.setLastname("Telles");
-        newUser.setAge(23);
-        newUser.setEmail("PabloTelles@gmail.com");
-        newUser.setPassword("spartan");
-        return newUser;
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable("id") Long idLong){
+        return userService.getUserById(idLong);
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createUser(@RequestBody User user){
+        userService.addUser(user);
+    }
 
-    
+    @PutMapping("/{username}")
+    public void updateUser(@PathVariable("username") String username, @RequestParam(required = false) String firstName, @RequestParam(required = false) String lastName, @RequestParam(required = false) Integer age, @RequestParam(required = false) String email, @RequestParam(required = false) String password){
+        userService.updateUser(username, firstName, lastName, age, email, password);
+    }
+
+    @DeleteMapping("/{username}")
+    public void deleteUser(@PathVariable("username") String username){
+        userService.deleteUserByUsername(username);
+    }       
 
 }
